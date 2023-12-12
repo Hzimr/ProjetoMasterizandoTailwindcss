772d571ac8684d2e8a6d85d4bca94f46076231bc
Hítalo Rodrigues <hitalo005@gmail.com>
2023-12-12 20:29:24 -0300

    feat: created breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="p-8 items-center bg-slate-900 text-slate-100 h-screen
     ">
-      <h1 className="flex items-center gap-3 font-bold text-5xl before:w-0.5 before:h-8 before:bg-sky-500">Hello Tailwind</h1>
+      <h1 className="flex items-center gap-3 font-bold text-3xl sm:text-5xl lg:text-6xl before:w-0.5 before:h-8 before:bg-sky-500">Hello Tailwind</h1>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
       <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
