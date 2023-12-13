d2da2706553d545a6c0f413631921a664e11ae6a
Hítalo Rodrigues <hitalo005@gmail.com>
2023-12-13 15:39:53 -0300

    feat: created config initial Prettier and Eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
-
 export default function Home() {
   return (
-    <div className="p-8 items-center bg-slate-900 text-slate-100 h-screen
-    ">
-      <h1 className="flex items-center gap-3 font-bold text-3xl sm:text-5xl lg:text-6xl before:w-0.5 before:h-8 before:bg-sky-500">Hello Tailwind</h1>
-      <h2>Hello Tailwind</h2>
-      <p>Hello Tailwind</p>
-      <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
-        Sign in
-      </button>
+    <div
+      className="flex h-screen flex-col items-center bg-slate-50 p-8
+    text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100"
+    >
+      <div className="max-w-app">
+        <h1 className="text-4xl font-bold sm:text-5xl lg:text-6xl">
+          Rapidly build modern websites without ever leaving your HTML.
+        </h1>
+        <p className="mt-4 dark:text-slate-400">
+          A utility-first CSS framework packed with classes like flex, pt-4,
+          text-center and rotate-90 that can be composed to build any design,
+          directly in your markup.
+        </p>
+        <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium text-white enabled:hover:bg-sky-600 disabled:cursor-not-allowed disabled:opacity-60 dark:bg-sky-400 dark:text-sky-950">
+          Sign in
+        </button>
+      </div>
     </div>
   )
 }
