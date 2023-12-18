330dd85a9e673b15dabc1efdf8b93cd42f21518c
Hítalo Rodrigues <hitalo005@gmail.com>
2023-12-18 18:51:04 -0300

    feat: created FileList

--- src/app/components/Form/FileInput/Control.tsx
+++ src/app/components/Form/FileInput/Control.tsx
@@ -4,8 +4,8 @@ import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
-export function Control(props: ControlProps) {
-  const { id, onFileSelected } = useFileInput()
+export function Control({ multiple = false, ...props }: ControlProps) {
+  const { id, onFilesSelected } = useFileInput()
 
   function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
     if (!event.target.files?.length) {
@@ -14,7 +14,7 @@ export function Control(props: ControlProps) {
 
     const files = Array.from(event.target.files)
 
-    onFileSelected(files)
+    onFilesSelected(files, multiple)
   }
 
   return (
@@ -23,6 +23,7 @@ export function Control(props: ControlProps) {
       type="file"
       id={id}
       onChange={handleFilesSelected}
+      multiple={multiple}
       {...props}
     />
   )
