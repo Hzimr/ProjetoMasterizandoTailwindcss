53baa40b1a694ec93cd762c50d65723a89b933eb
Hítalo Rodrigues <hitalo005@gmail.com>
2023-12-19 14:20:41 -0300

    fix: changed name Collpasible in Collapsible

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -7,22 +7,22 @@ import { Profile } from './Profile'
 import { InputControl, InputPrefix, InputRoot } from '../Input'
 import { Menu, Search } from 'lucide-react'
 import { NavItemFooterList, NavItemList } from '../constants/data'
-import * as Collpasible from '@radix-ui/react-collapsible'
+import * as Collapsible from '@radix-ui/react-collapsible'
 import { Button } from '../Button'
 
 export function Sidebar() {
   return (
-    <Collpasible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto  lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto  lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0">
       <div className="flex items-center justify-between">
         <Logo />
-        <Collpasible.Trigger asChild className="lg:hidden">
+        <Collapsible.Trigger asChild className="lg:hidden">
           <Button variant="ghost">
             <Menu className="h-6 w-6" />
           </Button>
-        </Collpasible.Trigger>
+        </Collapsible.Trigger>
       </div>
 
-      <Collpasible.Content
+      <Collapsible.Content
         forceMount
         className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex"
       >
@@ -42,7 +42,7 @@ export function Sidebar() {
 
           <Profile />
         </div>
-      </Collpasible.Content>
-    </Collpasible.Root>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
