9a30756ee92aef6599bcbeac47fad2b920ec3e92
Hítalo Rodrigues <hitalo005@gmail.com>
2023-12-20 16:59:23 -0300

    style: darktheme form

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -38,7 +38,7 @@ export function Sidebar() {
           <Navigation itemList={NavItemFooterList} />
 
           <UsedSpaceWidget />
-          <div className="h-px bg-zinc-200" />
+          <div className="h-px bg-zinc-200 dark:bg-zinc-700" />
 
           <Profile />
         </div>
